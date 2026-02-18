d92cda13415baf03849dee5e9c328aeec0653dd8
SantiagoRB17 <santiago.ramirezb1@uqvirtual.edu.co>
2026-02-18 08:01:48 -0500

    Refactor + Iterador

---
+++ src/main/java/co/edu/uniquindio/EliminarRConIterador.java
@@ -0,0 +1,28 @@
+package co.edu.uniquindio;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class EliminarRConIterador {
+    public static void main(String[] args) {
+        ArrayList<String> lista = new ArrayList<>();
+        lista.add("Santiago");
+        lista.add("Maria");
+        lista.add("Raul");
+        lista.add("Laura");
+        lista.add("Renteria");
+        eliminarR(lista);
+        System.out.println(lista);
+    }
+    public static void eliminarR(ArrayList<String> lista){
+        Iterator<String> it = lista.iterator();
+
+        while(it.hasNext()){
+            String nombre = it.next();
+            if(nombre.startsWith("R")){
+                it.remove();
+            }
+        }
+    }
+
+}
